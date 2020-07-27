183b244931045245821d0152dba766b2fb666c03
petebrown77 <petabrown77@gmail.com>
2020-07-27 10:23:23 -0400

    Create pymarkov.py
    Added simple code to get started

---
+++ pymarkov.py
@@ -0,0 +1,159 @@
+import numpy as np
+
+
+
+class MarkovChain(object):
+
+    def __init__(self, states, t_matrix=None):
+
+        self.states = states
+
+        self.index_dict = {self.states[index]: index for index in range(len(self.states))}
+
+
+
+        if t_matrix:
+
+            self.transistion_matrix = t_matrix
+
+
+
+
+
+    def generate_tmatrix(self, sequence):
+
+        """
+
+        Given a sequence, generate a transition matrix.
+
+        sequence: list -> list of values to generate matrix from.
+
+        """
+
+        n = len(set(sequence))
+
+
+
+        #To allow differnt types of data,
+
+        #We need to setup a dittionary
+
+        #That maps to each value within the sequence
+
+        seq_dict = {j: i for i, j in zip(list(range(n)), set(sequence))}
+
+
+
+        matrix = [[0] * n for _ in range(n)]
+
+
+
+        for (i, j) in zip(sequence, sequence[1:]):
+
+            matrix[seq_dict[i]][seq_dict[j]] += 1
+
+
+
+        for row in matrix:
+
+            s = sum(row)
+
+            if s > 0:
+
+                row[:] = [f / s for f in row]
+
+                
+
+        self.transistion_matrix = matrix
+
+
+
+        
+
+
+
+    def normalize_weights(self, weights):
+
+        """Python has a floating point problem,
+
+        where not every number is equal to the int.
+
+        eg. 0.1 + 0.2 + 0.7 = 1.000000001.
+
+
+
+        To solve this, we can normalize the weights before we use them.
+
+        """
+
+        weights /= np.sum(weights)
+
+        return weights
+
+
+
+    def next_state(self, current_state):
+
+ 
+
+        weights = self.transistion_matrix[self.index_dict[current_state]]
+
+        normalized = self.normalize_weights(weights)
+
+        
+
+        return np.random.choice(
+
+            self.states,
+
+            p=normalized
+
+            )
+
+
+
+    def generate_states(self, current_state, no=10):
+
+        future = []
+
+        for i in range(no):
+
+            next_state = self.next_state(current_state)
+
+            future.append(next_state)
+
+            current_state = next_state
+
+
+
+        return future
+
+
+
+
+
+if __name__ == "__main__":
+
+    matrix = [[0.8, 0.19, 0.1],
+
+              [0.2, 0.7, 0.1],
+
+              [0.1, 0.2, 0.7]]
+
+
+
+    chain = MarkovChain(["Sunny", "Rainy", "Snowy"], t_matrix=matrix)
+
+    print(chain.next_state("Snowy"))
+
+    sequence = chain.generate_states("Snowy", no=100)
+
+
+
+    newChain = MarkovChain(["Sunny", "Rainy", "Snowy"])
+
+    newChain.generate_tmatrix(sequence)
+
+
+
+    print(newChain.generate_states("Sunny", no=20))
